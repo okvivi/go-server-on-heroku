030547ab452705b59a8d081c3320750a6b5b6445
Octavian Costache <octavian.costache@gmail.com>
2013-04-24 12:08:30 -0400

    Formatting

--- src/okvivi/gofe/gofe.go
+++ src/okvivi/gofe/gofe.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "okvivi/models"
-  "flag"
+	"flag"
+	"fmt"
+	"net/http"
+	"okvivi/models"
 )
 
 var p = flag.Int("p", 3001, "The port to listen to")
 
 func hello(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, models.HelloWorld)
+	fmt.Fprint(w, models.HelloWorld)
 }
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  fmt.Println("Listening on", *p)
+	fmt.Println("Listening on", *p)
 
-  http.HandleFunc("/", hello)
-  err := http.ListenAndServe(fmt.Sprint(":", *p), nil)
-  if err != nil {
-    fmt.Println(err)
-  }
+	http.HandleFunc("/", hello)
+	err := http.ListenAndServe(fmt.Sprint(":", *p), nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
